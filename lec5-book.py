f5680d369ee7bb5b93414de66a4f57078c707482
summerdklee <leedakyung218@gmail.com>
2024-07-15 19:03:44 +0900

    lec5-book

--- lec5-book.py
+++ lec5-book.py
@@ -22,3 +22,52 @@ price_avg = sum(fruits['price']) / 3
 selling_amount_avg = sum(fruits['selling_amount']) / 3
 price_avg
 selling_amount_avg
+
+df_exam = pd.read_excel("data/excel_exam.xlsx")
+df_exam
+
+sum(df_exam["math"]) / 20
+sum(df_exam["english"])/ 20
+sum(df_exam["science"])/ 20
+
+len(df_exam) # 행 개수 반환
+df_exam.shape # (행 개수, 열 개수) 튜플로 반환
+df_exam.size # 행 개수 * 열 개수 반환
+
+df_exam["total"] = df_exam["math"] + df_exam["english"] + df_exam["science"]
+df_exam
+
+df_exam["mean"] = df_exam["total"] / 3
+df_exam
+
+df_exam[df_exam["math"] > 50] # math > 50가 True인 데이터만 추출됨
+df_exam[(df_exam["math"] > 50) & (df_exam["english"] > 50)]
+
+
+# 예제: 수학는 평균 이상인 학생 중 영어는 평균 이하인 학생은?
+mean_m = np.mean(df_exam['math'])
+mean_e = 
+
+df_exam[(df_exam["math"] > mean_m)]
+
+# 예제: 3반 학생 데이터만 추출
+df_exam['nclass'] == 3
+df_exam[df_exam['nclass'] == 3]
+
+# 예제: 3반 학생 데이터 중 math, english, science 데이터만 추출
+df_nc3 = df_exam[df_exam['nclass'] == 3]
+df_nc3[["math", 'english', 'science']]
+df_nc3[0:1]
+
+df_exam
+df_exam[7:16]
+df_exam[0::2]
+
+df_exam.sort_values('math', ascending = False)
+df_exam.sort_values(['nclass', 'math'], ascending = [True, False])
+
+np.where(a > 3) # 조건에 부합하는 요소의 위치를 찾아서 추출, 튜플 형태로 추출
+np.where(a > 3, "UP", "Down") # True면 Up, False면 Down 추출, np array 형태로 추출
+
+df_exam["updown"] = np.where(df_exam["math"] > 50, "Up", "Down")
+df_exam
